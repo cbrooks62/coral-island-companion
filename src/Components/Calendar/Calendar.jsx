52fca135b18aa11ad42efe9a802db24f1f7a88c5
Chelsea Brooks <cbrooks6212@gmail.com>
2024-06-27 13:50:20 -0400

    attempting new calendar feature

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,28 +1,37 @@
 export const Calendar = () => {
   return (
     <div>
-      <header className="calendar-header"></header>
+      <header className="calendar-header">
+        <img src="src/Images/Headers/Calendar-header1.png" />
+      </header>
       <div className="calendar-container">
-        <img
-          className="spring-calendar"
-          src={"src/Images/Calendar/Spring.png"}
-          alt="Spring Calendar"
-        />
-        <img
-          className="summer-calendar"
-          src={"src/Images/Calendar/Summer.png"}
-          alt="Summer Calendar"
-        />
-        <img
-          className="fall-calendar"
-          src={"src/Images/Calendar/Fall.png"}
-          alt="Fall Calendar"
-        />
-        <img
-          className="winter-calendar"
-          src={"src/Images/Calendar/Winter.png"}
-          alt="Winter Calendar"
-        />
+        <div className="spring-calendar">
+          <img
+            src={"src/Images/Calendar/Spring.png"}
+            alt="Spring Calendar"
+          />
+        </div>
+        <div>
+          <img
+            className="summer-calendar"
+            src={"src/Images/Calendar/Summer.png"}
+            alt="Summer Calendar"
+          />
+        </div>
+        <div>
+          <img
+            className="fall-calendar"
+            src={"src/Images/Calendar/Fall.png"}
+            alt="Fall Calendar"
+          />
+        </div>
+        <div>
+          <img
+            className="winter-calendar"
+            src={"src/Images/Calendar/Winter.png"}
+            alt="Winter Calendar"
+          />
+        </div>
       </div>
     </div>
   );
